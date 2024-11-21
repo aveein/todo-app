b6c8e96a2c8f7f6b4c6bd9a23970e628e2ebf598
aveein <avinamatya29@gmail.com>
2024-11-21 14:13:30 +0545

    addeed upcomming and done filters

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,26 +1,47 @@
 const { Todo } = require('../models')
+var ejs = require('ejs');
+const { Op } = require('sequelize');
 
-const { query, validationResult } = require('express-validator');
 
-
+//render index page
 const getTodos = (req, res) => {
 
     res.render('welcome');
 };
 
+//filter todo
 const  Filtertodos = async (req, res) => {
  
  
+   
    try{
-    const todos = await Todo.findAll();
-    let views = res.renderFile('')
-    res.status(200).json({data:views}); 
+    let todos = [];
+    if( req.query.status === '0' || req.query.status === '1') {
+         todos = await Todo.findAll({
+            where: {
+                status: {
+                  [Op.eq]: req.query.status,
+                },
+         }
+         });
+    }else{
+
+         todos = await Todo.findAll();
+    }
+    
+    ejs.renderFile('views/todos.ejs',{todos:todos}, function(err, data) {
+       
+        res.status(200).json({views:data});
+    });
+
+
+   
    }catch(error){
     res.status(500).json({message:error.message}); 
    }
 
 };
-
+//create todo
 const  createTodos = async (req, res) => {
  
     let data = req.body;
@@ -35,11 +56,88 @@ const  createTodos = async (req, res) => {
    }
 
 };
+//status update
+const updateStatus = async (req,res) => {
+    try{
+
+        const todo = await Todo.update({
+            status:req.body.status
+        },{
+            where: {
+              id: req.body.id,
+            },
+          });
+     
+        res.json({"status":"Updated Successfully"});
+
+       }catch(error){
+        res.status(500).json({message:error.message}); 
+       }
+}
+//edit todo
+const editTodo = async (req,res) => {
+    try{
+
+        const todo = await Todo.findByPk(req.params.id);
+          
+     
+
+        res.json(todo)
+
+       }catch(error){
+        res.status(500).json({message:error.message}); 
+       }
+}
+//update todo
+const updateTodo = async (req,res) => {
+    try{
+
+        const todo = await Todo.update({
+            name:req.body.title,
+            description:req.body.description,
+            due_date: req.body.due_date
+
+        },{
+            where: {
+              id: req.params.id
+            },
+          });
+     
+     
+          
+     
+
+        res.json(todo)
+
+       }catch(error){
+        res.status(500).json({message:error.message}); 
+       }
+}
+
+//delete todo
+const deleteTodo = async (req,res) => {
+
+    try{
+        const todo = await Todo.findByPk(req.params.id);
+ 
+        await todo.destroy();
+
+        res.json({'message':'Deleted Successfully'})
+
+    }catch(e){
+        res.status(500).json({message:error.message}); 
+    }
+  
 
 
+}
 module.exports={
     getTodos,
     createTodos,
-    Filtertodos
+    editTodo,
+    Filtertodos,
+    updateStatus,
+    updateTodo,
+    deleteTodo
 }
 
--- routes/route.js
+++ routes/route.js
@@ -3,16 +3,26 @@ const express = require('express')
 
 const router = express.Router();
 
-const {getTodos,createTodos} = require('../controllers/todoController.js');
-const { body } = require('express-validator');
+const {getTodos,createTodos, Filtertodos, updateStatus, editTodo, updateTodo, deleteTodo} = require('../controllers/todoController.js');
+
+//validators
 const { todoValidationRules, validate } = require('../requests/validator.js');
 
 
 router.get('/',getTodos);
-// todoValidationRules(),validate
+
+router.get('/filter-todos',Filtertodos);
+
 router.post('/create',todoValidationRules(),validate,createTodos);
 
+router.get('/edit/:id',editTodo);
 
+router.put('/update/:id',todoValidationRules(),validate,updateTodo);
+
+router.post('/update/status',updateStatus);
+
+
+router.delete('/delete/:id',deleteTodo);
 
 module.exports = router;
 
